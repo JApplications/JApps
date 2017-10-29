f4dfcb660ad3609c189851f02d8b2514f1b4e61a
JApplications <32767737+JApplications@users.noreply.github.com>
2017-10-29 20:14:11 +0100

    refactoring

---
+++ index.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="Jandro applications - software development">
+    <meta name="author" content="JApps">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Jandro Applications</title>
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="css/site.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="page-header">
+				<h1>
+					LayoutIt! <small>Interface Builder for Bootstrap</small>
+				</h1>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="page-header">
+				<h1>
+					LayoutIt! <small>Interface Builder for Bootstrap</small>
+				</h1>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="page-header">
+				<h1>
+					LayoutIt! <small>Interface Builder for Bootstrap</small>
+				</h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="page-header">
+						<h1>
+							LayoutIt! <small>Interface Builder for Bootstrap</small>
+						</h1>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="page-header">
+						<h1>
+							LayoutIt! <small>Interface Builder for Bootstrap</small>
+						</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-8">
+			<h2>
+				Heading
+			</h2>
+			<p>
+				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+			<h2>
+				Heading
+			</h2>
+			<p>
+				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+			</p>
+			<p>
+				<a class="btn" href="#">View details »</a>
+			</p>
+		</div>
+	</div>
+</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/jqueryui/jquery-ui.js"></script>
+</body>
+</html>
